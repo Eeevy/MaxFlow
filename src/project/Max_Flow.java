c1521b86b76cf12b2d837b9399bd1d9d198be4c6
Emma Shakespeare <emma@wikblad.com>
2015-11-06 16:48:00 +0100

    Fungerande bipartite matchning

--- src/project/Max_Flow.java
+++ src/project/Max_Flow.java
@@ -37,50 +37,59 @@ public class Max_Flow {
 		return pathFound;
 
 	}
-	
-	public int fordFulkersonAlgorithm(int[][] graph, int source, int sink){
+
+	public int fordFulkersonAlgorithm(int[][] graph, int source, int sink) {
 		int maxFlow = 0;
-		
-		while(bfs(graph, source, sink)){
+
+		while (bfs(graph, source, sink)) {
 			int v = sink;
 			int flowCapacity = Integer.MAX_VALUE;
-			while(v!= source){
+			while (v != source) {
 				int u = path[v];
 				int weight = graph[u][v];
 				flowCapacity = Math.min(flowCapacity, weight);
 				v = path[v];
-				
+
 			}
 			v = sink;
-			while(v!= source){
+			while (v != source) {
 				int u = path[v];
-				graph[v][u] -= flowCapacity;
-				graph[u][v] += flowCapacity;
+				graph[u][v] -= flowCapacity;
+				graph[v][u] += flowCapacity;
+				if (graph[v][u] > 0) {
+					if (v < 5 && u > 0) {
+						System.out.print("Mellan nod:" + u + "och " + v);
+						System.out.println();
+					}
+				}
+
 				v = path[v];
-				
-				
+
 			}
 			maxFlow += flowCapacity;
-			
+
 		}
 		return maxFlow;
-		
-		
+
 	}
 
 	public static void main(String[] args) {
-		int[][] graf = {
-				{0,1,1,0,0,0},
-				{0,0,0,0,1,0},
-				{0,0,0,0,0,0},
-				{0,0,0,0,0,1},
-				{0,0,0,0,0,1},
-				{0,0,0,0,0,0}
-		};
+		int[][] graf = { { 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 1, 0 },
+				{ 0, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0, 1 },
+				{ 0, 0, 0, 0, 0, 1 }, { 0, 0, 0, 0, 0, 0 } };
+
+		// int[][] graf = {
+		// {0,16,13,0,0,0},
+		// {0,0,10,12,0,0},
+		// {0,4,0,0,14,0},
+		// {0,0,9,0,0,20},
+		// {0,0,0,7,0,4},
+		// {0,0,0,0,0,0}
+		// };
 		Max_Flow run = new Max_Flow();
-		System.out.println("Maxflow för din graf är!:" + run.fordFulkersonAlgorithm(graf, 0, 5));
-		
+		System.out.println("Maxflow för din graf är!:"
+				+ run.fordFulkersonAlgorithm(graf, 0, 5));
+
 	}
-	
 
 }
