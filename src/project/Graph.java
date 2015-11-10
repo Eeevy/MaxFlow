8ae77b5aaa24a146dd4da4fccc81a7f463e5f0e3
Evelyn Gustavsson <Evy@Evelyns-MacBook-Air.local>
2015-11-10 21:36:04 +0100

    Har kommenterat lite till klasserna Graph och Max_Flow. Är däremot inte klar än. Tog bort BipartiteMatching-klassen eftersom den inte innehöll någon kod. ^

--- src/project/Graph.java
+++ src/project/Graph.java
@@ -1,6 +1,10 @@
 package project;
 
+/**
+ * Klass som initierar en tvådimensionell array som en graf med utrymme för 36 rutor.
+ * @author Emma Shakespeare och Evelyn Gustavsson
+ *
+ */
 public class Graph {
 	public int[][] graf = new int[6][6];
-
 }
--- src/project/Max_Flow.java
+++ src/project/Max_Flow.java
@@ -1,29 +1,55 @@
 package project;
 
-import java.util.ArrayList;
-/**
- * Initiera en graf med storleken som användaren matar in
- * createGraph(int leftSize, int rightSize )
- * addConnection(int left, int right)
- * 
- * Se till att det finns connection between source nodes and sink nodes
- * kommentera noggrant för att visa att du förstår
- * 
- */
 import java.util.LinkedList;
 
-public class Max_Flow {
-	private LinkedList<Integer> queue = new LinkedList<Integer>();
-	private int[][] graf = new int[6][6];
-	private int[] path = new int[6];
-	private boolean[] visited = new boolean[6];
+/*
+ * TODO 1: Initiera en graf med storleken som användaren matar in. Den ska heta createGraph(int leftSize, int rightSize).
+ */
 
+/*
+ * TODO 2: Se till att det finns en koppling mellan source-noder och sink-noder. Skapa metoden addConnection(int left, int right).
+ */
+
+/**
+ * Klassen hanterar bipartit matchning. En nod anses vara matchad om den ligger
+ * i en båge. I varje graf finns en graf finns en source-nod och en sink-nod och
+ * dessa utgör en del av flödesschemat. Det finns olika vägar mellan en
+ * source-nod och sink-nod, genom andra noder. Vid exekvering av programmet
+ * skrivs koppling mellan de mellanliggande noderna ut. Det är underförstått att
+ * vägen alltid börjar från en source-nod och avslutas vid en sink-nod. Därav
+ * sker ingen utskrift gällande de kopplingarna. Applikationen beräknar även det
+ * maximala flödet för den aktuella grafen.
+ * 
+ * @authors Emma Shakespeare och Evelyn Gustavsson
+ *
+ */
+public class Max_Flow {
+	// Instansvariabler.
+	private LinkedList<Integer> queue = new LinkedList<Integer>(); // Kö.
+	// private int[][] graf = new int[6][6];
+	private int[] path = new int[6]; // Väg.
+	private boolean[] visited = new boolean[6]; // Boolean som beskriver om en
+												// nod är besökt eller inte.
+
+	/**
+	 * Metod som utgår från principen Bredden-Först.
+	 * 
+	 * @param graph
+	 *            - en tvådimensionell heltalsarray som en graf
+	 * @param source
+	 *            - den nod som är längst till vänster.
+	 * @param sink
+	 *            - den nod som är längst till höger.
+	 * @return pathFound - Returnerar en boolean som är true om en väg hittas
+	 *         och false om den inte hittas.
+	 */
 	public boolean bfs(int[][] graph, int source, int sink) {
 		boolean pathFound = false;
 
 		for (int i = 0; i < graph.length; i++) {
 			visited[i] = false;
 		}
+
 		queue.add(source);
 		visited[source] = true;
 		path[source] = -1;
@@ -44,9 +70,21 @@ public class Max_Flow {
 			pathFound = true;
 		}
 		return pathFound;
-
 	}
 
+	/**
+	 * Metoden använder Ford Fulkersons algoritm för att hitta det maximala
+	 * flödet i nätverket.
+	 * 
+	 * @param graph
+	 *            - en tvådimensionell heltalsarray som en graf
+	 * 
+	 * @param source
+	 *            - den nod som är längst till vänster.
+	 * @param sink
+	 *            - den nod som är längst till höger.
+	 * @return maxFlow - det maximala flödet.
+	 */
 	public int fordFulkersonAlgorithm(int[][] graph, int source, int sink) {
 		int maxFlow = 0;
 
@@ -58,8 +96,8 @@ public class Max_Flow {
 				int weight = graph[u][v];
 				flowCapacity = Math.min(flowCapacity, weight);
 				v = path[v];
-
 			}
+
 			v = sink;
 			while (v != source) {
 				int u = path[v];
@@ -67,24 +105,45 @@ public class Max_Flow {
 				graph[v][u] += flowCapacity;
 				if (graph[v][u] > 0) {
 					if (v < sink && u > source) {
-						System.out.print("Mellan nod: " + u + "och " + v);
+						System.out.print("Koppling mellan nod: " + u + " och " + v);
 						System.out.println();
 					}
 				}
-
 				v = path[v];
-
 			}
 			maxFlow += flowCapacity;
-
 		}
 		return maxFlow;
+	}
+
+	/**
+	 * Initierar en graf med storleken som användaren matar in.
+	 * 
+	 * @param leftSize
+	 * @param rightSize
+	 */
+	public void createGraph(int leftSize, int rightSize) {
 
 	}
 
+	/**
+	 * Metod som ordnar så att det finns en koppling mellan source-noden och
+	 * sink-noden.
+	 * 
+	 * @param left
+	 * @param right
+	 */
+	public void addConnection(int left, int right) {
+
+	}
+
+	/**
+	 * Metod för att testköra programmet. Initierar grafen med 1:or och 0:or.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		int[][] graf = { { 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 1, 0 },
-				{ 0, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0, 1 },
+		int[][] graf = { { 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 1, 0 }, { 0, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0, 1 },
 				{ 0, 0, 0, 0, 0, 1 }, { 0, 0, 0, 0, 0, 0 } };
 
 		// int[][] graf = {
@@ -95,10 +154,8 @@ public class Max_Flow {
 		// {0,0,0,7,0,4},
 		// {0,0,0,0,0,0}
 		// };
+
 		Max_Flow run = new Max_Flow();
-		System.out.println("Maxflow för din graf är!:"
-				+ run.fordFulkersonAlgorithm(graf, 0, 5));
-
+		System.out.println("MaxFlow för din graf är: " + run.fordFulkersonAlgorithm(graf, 0, 5));
 	}
-
 }
